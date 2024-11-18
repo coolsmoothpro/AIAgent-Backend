17af305f0b2abfc640fe3531c2b127fe16f9b33b
coolsmoothpro <coolsmooth.pro@gmail.com>
2024-11-17 20:23:28 -0800

    import send_from_directory

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask import send_from_directory
 
 load_dotenv()
 
